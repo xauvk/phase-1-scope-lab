a7f92bb01876cd72f392eb8625deb9c8205d8d3c
xauvk <xauvkubalexanderyang@live.com>
2022-06-28 12:49:07 -0400

    commit tree 98cfd10bcbc3d20a9f10b36dc516b402109ce727

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+    let bestCustomer = 'not bob';
+}
+setBestCustomer()
+
+function overwriteBestCustomer() {
+    if (bestCustomer) {
+        bestCustomer = 'maybe bob'
+    } else {
+        let bestCustomer = 'maybe bob'
+    }
+}
+
+const leastFavoriteCustomer = 'matt'
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'connor'
+    return leastFavoriteCustomer
+}
